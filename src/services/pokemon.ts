4058e9597adcb4cb8183e0d20a42577e5b928784
leoeltavo2 <leo.azul2@hotmail.com>
2024-11-13 16:18:26 -0600

    Texto dinámico

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,15 +1,22 @@
 // Obtener el pokemon
-export const getPokemon = async({name}) => {
+export const getPokemon = async({name}: { name: String }) => {
     const pokemonDetails = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const detail = await pokemonDetails.json();    
     return detail;
 }
 
 // Obtener el texto del pokemon 
-export const getPokemonSpecie = async({id}) => {
+export const getPokemonSpecie = async({id}: { id: number }) => {
     const pokemonSpecies = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
     const specie = await pokemonSpecies.json(); 
     return specie;
+}
+
+// Obtener el lenguage
+export const getLanguage = async({id}: { id: number }) => {
+    const languages = await fetch(`https://pokeapi.co/api/v2/language/${id}`);
+    const language = await languages.json(); 
+    return language;
 }
 
 // Obtener la lista de pokemon
